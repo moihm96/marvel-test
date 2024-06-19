3b4d0a73abe422f0af42f28359f79cf24262d4a5
moihm96 <moihermol@gmail.com>
2024-06-19 20:18:55 +0100

    Adding control error for not found character

---
+++ src/app/character/[id]/error.tsx
@@ -0,0 +1,24 @@
+'use client'; // Error components must be Client Components
+
+import { useEffect } from 'react';
+import styles from '../../../styles/error.module.css';
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div className={styles.errorContainer}>
+      <h2>{error.message}</h2>
+      <p>Please try another id</p>
+    </div>
+  );
+}
