1a7db5afeff34b28fd31eaf4fad71d2f5a1fb4a8
moihm96 <moihermol@gmail.com>
2024-06-19 20:14:21 +0100

    Adding global errors handling

--- src/data/api.tsx
+++ src/data/api.tsx
@@ -16,38 +16,52 @@ export const urlCharacters = `${API_URL}/characters?limit=${limit}&${query}`;
 
 export const fetchCharacters = async () => {
   const response = await fetch(urlCharacters);
-  const data = await response.json();
+  try {
+    const data = await response.json();
 
-  return {
-    characters: data.data.results as Character[],
-    count: data.data.count as number,
-  };
+    return {
+      characters: data.data.results as Character[],
+      count: data.data.count as number,
+    };
+  } catch (error) {
+    throw new Error(`There is a problem fetching data from Marvel's APi`);
+  }
 };
 
 export const searchCharacters = async (querySearch: string) => {
   const url = `${API_URL}/characters?nameStartsWith=${querySearch}&limit=${limit}&${query}`;
-  const response = await fetch(url);
-  const data = await response.json();
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
 
-  return {
-    characters: data.data.results as Character[],
-    count: data.data.count as number,
-  };
+    return {
+      characters: data.data.results as Character[],
+      count: data.data.count as number,
+    };
+  } catch (error) {
+    throw new Error(
+      'There is no character for the search or network search is not working properly',
+    );
+  }
 };
 
 export const fetchCharacter = async (id: string) => {
   const url = `${API_URL}/characters/${id}?${query}`;
 
-  const responseCharacter = await fetch(url);
-  const character = await responseCharacter.json();
+  try {
+    const responseCharacter = await fetch(url);
+    const character = await responseCharacter.json();
 
-  const urlComic = `${API_URL}/characters/${id}/comics?limit=20&${query}`;
+    const urlComic = `${API_URL}/characters/${id}/comics?limit=20&${query}`;
 
-  const responseComics = await fetch(urlComic);
-  const comics = await responseComics.json();
+    const responseComics = await fetch(urlComic);
+    const comics = await responseComics.json();
 
-  return {
-    character: character.data.results[0] as Character,
-    comics: comics.data.results as Comic[],
-  };
+    return {
+      character: character.data.results[0] as Character,
+      comics: comics.data.results as Comic[],
+    };
+  } catch (error) {
+    throw new Error('Character not found or bad typed');
+  }
 };
