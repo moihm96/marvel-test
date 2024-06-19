ba0ea8f8ff5f6d6958d94bfbe5b3de94eb6e8733
moihm96 <moihermol@gmail.com>
2024-06-19 20:52:33 +0100

    Removing count from main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,6 @@ import Characters from '@/components/Characters';
 import { fetchCharacters } from '@/data/api';
 
 export default async function Home() {
-  const { characters, count } = await fetchCharacters();
-  return <Characters characters={characters} count={count} />;
+  const { characters } = await fetchCharacters();
+  return <Characters characters={characters} />;
 }
